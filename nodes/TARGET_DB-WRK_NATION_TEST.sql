c90cd8e4e1b9078a08cc661ef797886149ceb5db
Tanvi Kamat <tanvi.kamat.contractor@coalesce.io>
2026-07-31 16:26:13 +0530

    deployment

---
+++ nodes/TARGET_DB-WRK_NATION_TEST.sql
@@ -0,0 +1,16 @@
+@id("1694b8d8-b439-466a-9ce0-be126ae69f27")
+@nodeType("696")
+@clusterKey(true)
+@truncateBefore(false)
+@clusterKeyConfig(" trunc(""N_NATIONKEY"", -5)", "[object Object]")
+@description("adf''kgjt")
+@selectDistinct(true)
+@orderby(true)
+@orderbycolumn("[object Object]", "desc")
+SELECT
+     "N_NATIONKEY" AS "N_NATIONKEY" @nullable(false),
+     "N_NAME" AS "N_NAME" @description("jkadfh''adfjh"),
+     "N_REGIONKEY" AS "N_REGIONKEY" @defaultValue(0),
+     "N_COMMENT" AS "N_COMMENT",
+     "N_LOAD_TIMESTAMP" AS "N_LOAD_TIMESTAMP"
+FROM {{ ref('SOURCE_DATA', 'NATION_TEST') }} "NATION_TEST"
