a7c5b04f78b84bdccceebd5f3fe257159f2d3e48
Tanvi Kamat <tanvi.kamat.contractor@coalesce.io>
2026-08-03 12:42:36 +0530

    cluster By - add

--- nodes/TARGET_DB-WRK_NATION_TEST.sql
+++ nodes/TARGET_DB-WRK_NATION_TEST.sql
@@ -3,6 +3,7 @@
 @clusterKey(true)
 @truncateBefore(false)
 @clusterKeyConfig("trunc(""N_NATIONKEY"", -5)", "[object Object]")
+@clusterKeyConfig("trunc(""N_NATIONKEY"", -5)", "[object Object]")
 @description("adf''kgjt - edited")
 @selectDistinct(true)
 @materializationType("transient table")
