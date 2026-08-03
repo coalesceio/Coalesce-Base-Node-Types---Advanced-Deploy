6ce03b495da9fd0bb8035c6be6b04bb35b231eba
Tanvi Kamat <tanvi.kamat.contractor@coalesce.io>
2026-08-03 16:36:36 +0530

    testing alias

--- nodes/TARGET_DB-WRK_NATION_TEST.sql
+++ nodes/TARGET_DB-WRK_NATION_TEST.sql
@@ -5,6 +5,8 @@
 @description("adf''kgjt - edited")
 @selectDistinct(true)
 @materializationType("transient table")
+@asOfJoin(true)
+@asofJoinConfig("SOURCE_DATA", "NATION_HISTORY", "NATION_TEST.N_LOAD_TIMESTAMP>=NATION_HISTORY.N_LOAD_TIMESTAMP", "ON", "NATION_TEST.N_NATIONKEY = NATION_HISTORY.N_NATIONKEY")
 SELECT
      "N_NATIONKEY" AS "N_NATIONKEY" @inHash("1|GH_COL") @description("jkdfh''afjh") @defaultValue(1),
      "N_NAME"::VARCHAR(200) AS "N_NAME" @nullable(false) @description("jkafh''afjh"),
@@ -12,4 +14,4 @@ SELECT
      "N_LOAD_TIMESTAMP" AS "N_LOAD_TIMESTAMP"  @nullable(true) ,
      {{ get_hash("GH_COL") }}::string AS "GH_COL" @nullable(true)
 FROM {{ ref('SOURCE_DATA', 'NATION_TEST') }} "NATION_TEST"
-WHERE N_NATIONKEY = 2
+WHERE NATION_TEST.N_NATIONKEY = 2
