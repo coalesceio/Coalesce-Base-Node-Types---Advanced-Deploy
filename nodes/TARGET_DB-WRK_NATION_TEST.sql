fedde022905a0e8734fe79c8ba1961fd9bb9213d
Tanvi Kamat <tanvi.kamat.contractor@coalesce.io>
2026-08-03 12:22:54 +0530

    string 100

--- nodes/TARGET_DB-WRK_NATION_TEST.sql
+++ nodes/TARGET_DB-WRK_NATION_TEST.sql
@@ -5,9 +5,10 @@
 @clusterKeyConfig("trunc(""N_NATIONKEY"", -5)", "[object Object]")
 @description("adf''kgjt - edited")
 @selectDistinct(true)
+@materializationType("transient table")
 SELECT
      "N_NATIONKEY" AS "N_NATIONKEY" @inHash("1|GH_COL") @description("jkdfh''afjh") @defaultValue(1),
-     "N_NAME" AS "N_NAME" @nullable(false) @description("jkafh''afjh"),
+     "N_NAME"::VARCHAR(100) AS "N_NAME" @nullable(false) @description("jkafh''afjh"),
      "N_REGIONKEY" AS "N_REGIONKEY_RENAME" @defaultValue(2) @previousName("N_REGIONKEY"),
      "N_LOAD_TIMESTAMP" AS "N_LOAD_TIMESTAMP"  @nullable(true) ,
      {{ get_hash("GH_COL") }}::string AS "GH_COL" @nullable(true)
