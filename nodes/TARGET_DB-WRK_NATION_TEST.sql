8658f52f2502c389ccb30d54d3480b34f4c19da2
Tanvi Kamat <tanvi.kamat.contractor@coalesce.io>
2026-08-03 12:09:42 +0530

    null - drop

--- nodes/TARGET_DB-WRK_NATION_TEST.sql
+++ nodes/TARGET_DB-WRK_NATION_TEST.sql
@@ -7,7 +7,7 @@
 @selectDistinct(true)
 SELECT
      "N_NATIONKEY" AS "N_NATIONKEY" @inHash("1|GH_COL") @description("jkdfh''afjh") @defaultValue(1),
-     "N_NAME" AS "N_NAME" @nullable(false) @description("jkafh''afjh") @defaultValue(1),
+     "N_NAME" AS "N_NAME" @nullable(false) @description("jkafh''afjh"),
      "N_REGIONKEY" AS "N_REGIONKEY_RENAME" @defaultValue(1) @previousName("N_REGIONKEY"),
      "N_LOAD_TIMESTAMP" AS "N_LOAD_TIMESTAMP"  @nullable(true) ,
      {{ get_hash("GH_COL") }}::string AS "GH_COL" @nullable(true)
