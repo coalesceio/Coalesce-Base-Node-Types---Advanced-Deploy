5c04b0b19be3890fb9ec2030156a22189544488c
Tanvi Kamat <tanvi.kamat.contractor@coalesce.io>
2026-08-03 10:19:05 +0530

    add and drop column

--- nodes/TARGET_DB-WRK_NATION_TEST.sql
+++ nodes/TARGET_DB-WRK_NATION_TEST.sql
@@ -5,13 +5,10 @@
 @clusterKeyConfig("trunc(""N_NATIONKEY"", -5)", "[object Object]")
 @description("adf''kgjt - edited")
 @selectDistinct(true)
-@orderby(true)
-@orderbycolumn("[object Object]", "desc")
-@orderbycolumn("[object Object]", "desc")
 SELECT
-     "N_NATIONKEY" AS "N_NATIONKEY" @nullable(false),
+     "N_NATIONKEY" AS "N_NATIONKEY" @nullable(false) @inHash("1|GH_COL"),
      "N_NAME" AS "N_NAME" @description("jkadfh''adfjh"),
      "N_REGIONKEY" AS "N_REGIONKEY" @defaultValue(0),
-     "N_COMMENT" AS "N_COMMENT",
-     "N_LOAD_TIMESTAMP" AS "N_LOAD_TIMESTAMP"
+     "N_LOAD_TIMESTAMP" AS "N_LOAD_TIMESTAMP",
+     {{ get_hash(GH_COL) }}::string AS "GH_COL"
 FROM {{ ref('SOURCE_DATA', 'NATION_TEST') }} "NATION_TEST"
