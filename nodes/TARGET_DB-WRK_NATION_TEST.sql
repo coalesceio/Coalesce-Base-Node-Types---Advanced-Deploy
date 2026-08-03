5200bc6d9bdb7e051ff52bb98d684a04116514a7
Tanvi Kamat <tanvi.kamat.contractor@coalesce.io>
2026-08-03 12:44:51 +0530

    cluster - drop

--- nodes/TARGET_DB-WRK_NATION_TEST.sql
+++ nodes/TARGET_DB-WRK_NATION_TEST.sql
@@ -1,6 +1,5 @@
 @id("1694b8d8-b439-466a-9ce0-be126ae69f27")
 @nodeType("696")
-@clusterKey(true)
 @truncateBefore(false)
 @clusterKeyConfig("trunc(""N_NATIONKEY"", -5)", "[object Object]")
 @description("adf''kgjt - edited")
