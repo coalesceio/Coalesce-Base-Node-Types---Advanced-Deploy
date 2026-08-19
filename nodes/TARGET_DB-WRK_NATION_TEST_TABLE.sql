14d10b5a97d358bf8f00b9641c1623b9e7b73870
Tanvi Kamat <tanvi.kamat.contractor@coalesce.io>
2026-08-19 17:58:43 +0530

    Redeployment -> View recreate, Transient -> Modify, Table -> drop Column

--- nodes/TARGET_DB-WRK_NATION_TEST_TABLE.sql
+++ nodes/TARGET_DB-WRK_NATION_TEST_TABLE.sql
@@ -9,15 +9,14 @@
 @tests("SELECT 4 FROM {{ this }}", "After", true)
 @selectDistinct
 @description("Table '''decsripton --adfk")
---@preSQL("SELECT 1 FROM {{ this }} GROUP BY N_COMMENT HAVING COUNT(*) > 1")
---@preSQL("SELECT 1 FROM {{ this }} GROUP BY N_COMMENT HAVING COUNT(*) > 2")
---@postSQL("SELECT 1 FROM {{ this }} GROUP BY N_COMMENT HAVING COUNT(*) > 1")
---@postSQL("SELECT 1 FROM {{ this }} GROUP BY N_COMMENT HAVING COUNT(*) > 2")
+@preSQL("SELECT 1 FROM {{ this }} GROUP BY N_COMMENT HAVING COUNT(*) > 1")
+@preSQL("SELECT 1 FROM {{ this }} GROUP BY N_COMMENT HAVING COUNT(*) > 2")
+@postSQL("SELECT 1 FROM {{ this }} GROUP BY N_COMMENT HAVING COUNT(*) > 1")
+@postSQL("SELECT 1 FROM {{ this }} GROUP BY N_COMMENT HAVING COUNT(*) > 2")
 SELECT
      "N_NATIONKEY" AS "N_NATIONKEY" @isClusterKey(5) @description("Column '''decsripton --adfk") @inHash("GH_COL", 1),
      "N_NAME" AS "N_NAME" @isClusterKey(6) @inHash("GH_COL", 2) @notNull @tests("null") @tests("unique"),
      "N_REGIONKEY" AS "N_REGIONKEY" @previousName("N_REGIONKEY") @inHash("GH_COL", 3) @inHash("GH_COL", 4) @tests("null"),
-     "N_COMMENT" AS "N_COMMENT" @tests("unique") @defaultValue("N/A"),
      "N_LOAD_TIMESTAMP" AS "N_LOAD_TIMESTAMP" @isClusterKey(2, "DATE_TRUNC('MONTH', N_LOAD_TIMESTAMP)") @isClusterKey(3, "DATE_TRUNC('YEAR', N_LOAD_TIMESTAMP)") @isClusterKey(4, "TO_DATE(N_LOAD_TIMESTAMP)"),
      {{get_hash("GH_COL")}}::STRING AS HASH_COL,
      {{get_hash("GH_COL", algo='SHA256', delimiter='-') }}::STRING AS HASH_COL_SHA256,
--- nodes/TARGET_DB-WRK_NATION_TEST_TTABLE.sql
+++ nodes/TARGET_DB-WRK_NATION_TEST_TTABLE.sql
@@ -1,13 +1,24 @@
 @id("609a867c-66ca-4d7e-8f52-ecd7653071e3")
 @nodeType("702")
-@clusterEnabled
 @materializationType("transient table")
+@clusterEnabled
+@truncateBefore
+@testsEnabled
+@tests("SELECT 1 FROM {{ this }}")
+@tests("SELECT 2 FROM {{ this }}", "Before", true)
+@tests("SELECT 3 FROM {{ this }}", "After")
+@tests("SELECT 4 FROM {{ this }}", "After", true)
+@selectDistinct
 @description("Table '''decsripton --adfk")
+@preSQL("SELECT 1 FROM {{ this }} GROUP BY N_COMMENT HAVING COUNT(*) > 1")
+@preSQL("SELECT 1 FROM {{ this }} GROUP BY N_COMMENT HAVING COUNT(*) > 2")
+@postSQL("SELECT 1 FROM {{ this }} GROUP BY N_COMMENT HAVING COUNT(*) > 1")
+@postSQL("SELECT 1 FROM {{ this }} GROUP BY N_COMMENT HAVING COUNT(*) > 2")
 SELECT
      "N_NATIONKEY" AS "N_NATIONKEY" @isClusterKey(5) @description("Column '''decsripton --adfk") @inHash("GH_COL", 1),
-     "N_NAME" AS "N_NAME" @isClusterKey(6) @inHash("GH_COL", 2),
-     "N_REGIONKEY" AS "N_REGIONKEY" @previousName("N_REGIONKEY") @inHash("GH_COL", 3),
-     "N_COMMENT" AS "N_COMMENT",
+     "N_NAME" AS "N_NAME" @isClusterKey(6) @inHash("GH_COL", 2) @notNull @tests("null") @tests("unique"),
+     "N_REGIONKEY" AS "N_REGIONKEY" @previousName("N_REGIONKEY") @inHash("GH_COL", 3) @inHash("GH_COL", 4) @tests("null"),
+     "N_COMMENT" AS "N_COMMENT" @tests("unique") @defaultValue("N/A"),
      "N_LOAD_TIMESTAMP" AS "N_LOAD_TIMESTAMP" @isClusterKey(2, "DATE_TRUNC('MONTH', N_LOAD_TIMESTAMP)") @isClusterKey(3, "DATE_TRUNC('YEAR', N_LOAD_TIMESTAMP)") @isClusterKey(4, "TO_DATE(N_LOAD_TIMESTAMP)"),
      {{get_hash("GH_COL")}}::STRING AS HASH_COL,
      {{get_hash("GH_COL", algo='SHA256', delimiter='-') }}::STRING AS HASH_COL_SHA256,
