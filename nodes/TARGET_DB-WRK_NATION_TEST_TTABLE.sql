e14bdd37a4aa4618db610484f319cd042706facc
Tanvi Kamat <tanvi.kamat.contractor@coalesce.io>
2026-08-19 18:11:31 +0530

    Table -> Add column with multi details

--- nodes/TARGET_DB-WRK_NATION_TEST_TTABLE.sql
+++ nodes/TARGET_DB-WRK_NATION_TEST_TTABLE.sql
@@ -18,7 +18,6 @@ SELECT
      "N_NATIONKEY" AS "N_NATIONKEY" @isClusterKey(5) @description("Column '''decsripton --adfk") @inHash("GH_COL", 1),
      "N_NAME" AS "N_NAME" @isClusterKey(6) @inHash("GH_COL", 2) @notNull @tests("null") @tests("unique"),
      "N_REGIONKEY" AS "N_REGIONKEY" @inHash("GH_COL", 3) @inHash("GH_COL", 4) @tests("null"),
-     "N_COMMENT" AS "N_COMMENT" @tests("unique") @defaultValue("N/A"),
      "N_LOAD_TIMESTAMP" AS "N_LOAD_TIMESTAMP" @isClusterKey(2, "DATE_TRUNC('MONTH', N_LOAD_TIMESTAMP)") @isClusterKey(3, "DATE_TRUNC('YEAR', N_LOAD_TIMESTAMP)") @isClusterKey(4, "TO_DATE(N_LOAD_TIMESTAMP)"),
      {{get_hash("GH_COL")}}::STRING AS HASH_COL,
      {{get_hash("GH_COL", algo='SHA256', delimiter='-') }}::STRING AS HASH_COL_SHA256,
