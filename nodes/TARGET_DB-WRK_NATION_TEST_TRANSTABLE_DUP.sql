efbf4000c6be2447262e2d6e5a9242aebfc995ac
Tanvi Kamat <tanvi.kamat.contractor@coalesce.io>
2026-08-20 16:08:55 +0530

    Trans Duplicate

---
+++ nodes/TARGET_DB-WRK_NATION_TEST_TRANSTABLE_DUP.sql
@@ -0,0 +1,27 @@
+@id("05eff767-7700-42d1-9c23-0585f0817eed")
+@nodeType("702")
+@materializationType("transient table")
+@clusterEnabled
+@truncateBefore
+@testsEnabled
+@tests("SELECT 1 FROM {{ this }}")
+@tests("SELECT 2 FROM {{ this }}", "Before", true)
+@tests("SELECT 3 FROM {{ this }}", "After")
+@tests("SELECT 4 FROM {{ this }}", "After", true)
+@selectDistinct
+@description("Table '''decsripton --adfk")
+@preSQL("SELECT 1 FROM {{ this }} GROUP BY N_LOAD_TIMESTAMP HAVING COUNT(*) > 1")
+@preSQL("SELECT 1 FROM {{ this }} GROUP BY N_LOAD_TIMESTAMP HAVING COUNT(*) > 2")
+@postSQL("SELECT 1 FROM {{ this }} GROUP BY N_LOAD_TIMESTAMP HAVING COUNT(*) > 1")
+@postSQL("SELECT 1 FROM {{ this }} GROUP BY N_LOAD_TIMESTAMP HAVING COUNT(*) > 2")
+SELECT
+     "N_NATIONKEY" AS "N_NATIONKEY" @isClusterKey(5) @description("Column '''decsripton --adfk") @inHash("GH_COL", 1),
+     "N_NAME" AS "N_NAME" @isClusterKey(6) @inHash("GH_COL", 2) @notNull @tests("null") @tests("unique"),
+     "N_REGIONKEY" AS "N_REGIONKEY" @inHash("GH_COL", 3) @inHash("GH_COL", 4) @tests("null"),
+     "N_COMMENT" AS "N_COMMENT" @tests("unique"),
+     "N_LOAD_TIMESTAMP" AS "N_LOAD_TIMESTAMP" @isClusterKey(2, "DATE_TRUNC('MONTH', N_LOAD_TIMESTAMP)") @isClusterKey(3, "DATE_TRUNC('YEAR', N_LOAD_TIMESTAMP)") @isClusterKey(4, "TO_DATE(N_LOAD_TIMESTAMP)"),
+     {{get_hash("GH_COL")}}::STRING AS HASH_COL,
+     {{get_hash("GH_COL", algo='SHA256', delimiter='-') }}::STRING AS HASH_COL_SHA256,
+     {{get_hash("GH_COL", algo='MD5', delimiter='&') }}::STRING AS HASH_COL_MD5,
+     {{get_hash("GH_COL", delimiter='&') }}::STRING AS HASH_COL_DELIM
+FROM {{ ref('SOURCE_DATA', 'NATION_TEST') }} "NATION_TEST"
